68dd9b6add118da6f61864c814ea736044e9d0ae
Nathan Sinclair <n.b.sinclair.7@gmail.com>
2026-03-14 18:25:17 -0500

    Completed check out functionality

--- src/CapsuleHotel.java
+++ src/CapsuleHotel.java
@@ -45,7 +45,6 @@ public class CapsuleHotel {
      * @param prompt the message to display
      * @return a non-null, non-empty, non-whitespace string
      */
-
     public static String readString(Scanner console, String prompt) {
         String input;
         boolean isValid = false;
@@ -75,7 +74,6 @@ public class CapsuleHotel {
      * @param prompt the message to display
      * @return a non-negative integer
      */
-
     public static int readInt(Scanner console, String prompt) {
         String inputStr;
         boolean isValid = false;
@@ -102,21 +100,25 @@ public class CapsuleHotel {
      * method to handle menu display and options.
      * @param console the Scanner object to use for input
      */
-
     public static void menu(Scanner console) {
         boolean exit = false;
 
         do {
             String option = getMenuOption(console);
 
+            boolean success;
+
             switch (option) {
                 case "1":
                     System.out.println("\nGuest Check In");
                     System.out.println("==============");
                     String guestName = readString(console,"Guest Name: ");
-                    boolean success = handleCheckIn(console, guestName);
+                    success = handleCheckIn(console, guestName);
                     break;
                 case "2":
+                    System.out.println("\nGuest Check Out");
+                    System.out.println("===============");
+                    success = handleCheckOut(console);
                     break;
                 case "3":
                     break;
@@ -137,7 +139,6 @@ public class CapsuleHotel {
      * @param console the Scanner object to use for input
      * @return String the selected menu option
      */
-
     public static String getMenuOption(Scanner console) {
 
         System.out.println("\nGuest menu");
@@ -169,7 +170,6 @@ public class CapsuleHotel {
      * Error :(
      * Capsule #5 is occupied. (5 is an example, should be replaced by the capsule number the user tries to input)
      */
-
     public static boolean handleCheckIn(Scanner console, String guestName) {
         int capsuleNum = readInt(console, "Capsule #[1-" + capsulesAvailable + "]: ");
         int capsuleI = capsuleNum - 1;
@@ -183,11 +183,98 @@ public class CapsuleHotel {
             System.out.println("Error :(");
             System.out.println("Capsule #" + capsuleNum + " is occupied.");
             return false;
+        } else {
+            capsules[capsuleI] = guestName;
+            System.out.println("\nSuccess :)");
+            System.out.printf("%s is booked in capsule #%d\n", guestName, capsuleNum);
+            return true;
         }
-        // Shouldn't reach this code unless capsule is a valid option
-        capsules[capsuleI] = guestName;
-        System.out.println("\nSuccess :)");
-        System.out.printf("%s is booked in capsule #%d\n", guestName, capsuleNum);
-        return true;
+    }
+
+    /**
+     * Method Name: handleCheckOut
+     * The handleCheckOut method is designed to manage the check-out process for guests from capsules. It takes a Scanner object for user input and returns a boolean value indicating the success or failure of the check-out process.
+     * @param console the Scanner object to use for input
+     * @return boolean
+     * If no guests are checked into the hotel the user should see the following error message:
+     * Sorry... check out is only available if there's at least one guest.
+     * If the capsule number does not exist the user should see the following error message:
+     * Error :(
+     * Capsule #9 does not exist. (9 is an example, should be replaced by the capsule number the user tries to input)
+     * If the Guest is successfully booked the user should see the following success message:
+     * Success :)
+     * John checked out from capsule #3 (John and 3 are examples, they should be replaced by the user inputs)
+     * If the capsule is unoccupied the user should see the following error message:
+     * Error :(
+     * Capsule #5 is unoccupied. (5 is an example, should be replaced by the capsule number the user tries to input)
+     */
+    public static boolean handleCheckOut(Scanner console) {
+        boolean isEmpty = true;
+
+        for (int i = 0; i < capsules.length; i++) {
+            if (capsules[i] != null) { // There is a guest
+                isEmpty = false;
+                break;
+            }
+        }
+
+        if (isEmpty) {
+            System.out.println("Sorry... check out is only available if there's at least one guest.");
+            return false;
+        }
+
+        int capsuleNum = readInt(console, "Capsule #[1-" + capsulesAvailable + "]: ");
+        int capsuleI = capsuleNum - 1;
+
+        if (capsuleI < 0 || capsuleI >= capsulesAvailable) {
+            System.out.println("Error :(");
+            System.out.println("Capsule #" + capsuleNum + " does not exist");
+            return false;
+        }
+
+        if (capsules[capsuleI] != null) {
+            String guestName = capsules[capsuleI];
+            capsules[capsuleI] = null;
+
+            System.out.println("Success :)");
+            System.out.printf("%s checked out from capsule #%d\n", guestName, capsuleNum);
+            return true;
+        } else {
+            System.out.println("Error :(");
+            System.out.println("Capsule #" + capsuleNum + " is unoccupied");
+            return false;
+        }
+
+    }
+
+    /**
+     * Method Name: viewGuests
+     * The viewGuests method is designed to display information about the guests occupying capsules.
+     * It takes a Scanner object for user input and prints a list of guests in capsules along with their capsule numbers,
+     * if the capsule is null [unoccupied] should be printed, otherwise the guest name should be printed.
+     * @param console the Scanner object to use for input
+     * Example Output:
+     * Capsule: Guest
+     * 1: John
+     * 2: [unoccupied]
+     * 3: [unoccupied]
+     * 4: [unoccupied]
+     * 5: [unoccupied]
+     * 6: [unoccupied]
+     * 7: [unoccupied]
+     * 8: [unoccupied]
+     * 9: [unoccupied]
+     * 10: [unoccupied]
+     * 11: [unoccupied]
+     *
+     * 11 guests should always be displayed. If the user inputs a number at the end of the array the last 11 capsules should be displayed. If the user enters a number at the beginning of the array the first 11 capsules should be displayed. Otherwise the capsule number entered and the 5 below it and 5 above it should be displayed.
+     *
+     * If the user enters a capsule number that does not exist the user should see the following error message:
+     * Error :(
+     * Capsule #51 does not exist. (51 is an example, this number should be replaced with the user input)
+     */
+    public static void viewGuests(Scanner console) {
+        int capsuleNum = readInt(console, "Capsule #[1-" + capsulesAvailable + "]: ");
+        int capsuleI = capsuleNum - 1;
     }
 }
